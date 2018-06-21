a8dc2c8aff27ceef1e3d62a0b20938465f7cf6e2
ridhaaDev <ridhaadev@gmail.com>
2018-06-20 17:02:36 -0700

    Most of home page done, including carousel

--- js/script.js
+++ js/script.js
@@ -15,16 +15,3 @@ window.onload = function() {
   var ball = document.getElementById('moreLinks');
   ball.style.animation="roll 1.5s ease-in-out";
 }
-
-
-
-
-window.onscroll = function() {scrollEffect()};
-
-function scrollEffect() {
-    if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 350) {
-        // document.getElementById("myImg").className = "slideUp";
-        var mainNav=document.getElementById("mainNav");
-        mainNav.style.height="50px";
-    }
-}
