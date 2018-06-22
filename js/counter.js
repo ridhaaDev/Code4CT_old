715896f6641151e8172ae0ba6b8be8335d18106f
ridhaaDev <ridhaadev@gmail.com>
2018-06-22 16:50:10 -0700

    Completed the contact page and almost the portfolio page

---
+++ js/counter.js
@@ -0,0 +1,30 @@
+var timeLeftForCoffee=2500;
+var timeLeftForGirls=280;
+var timeLeftForProjects=2680;
+var timeLeftForHours=8265;
+
+var cupsOfCoffee=document.getElementById("cupsOfCoffee");
+var girls=document.getElementById("girls");
+var projects=document.getElementById("projects");
+var hoursCoding=document.getElementById("hoursCoding");
+
+    var timer=setInterval(function(){
+      cupsOfCoffee.innerHTML=timeLeftForCoffee;
+      girls.innerHTML=timeLeftForGirls;
+      projects.innerHTML=timeLeftForProjects;
+      hoursCoding.innerHTML=timeLeftForHours;
+
+      if (timeLeftForCoffee===3000) {
+      clearInterval(timer);
+      cupsOfCoffee.innerHTML="3000+";
+      girls.innerHTML="780+";
+      projects.innerHTML="3120+";
+      hoursCoding.innerHTML="8765+";
+
+      }
+      timeLeftForGirls++;
+      timeLeftForCoffee++;
+      timeLeftForProjects++;
+      timeLeftForHours++;
+    },
+    1);
